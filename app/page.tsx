c85b4ea9b7b13e04722cc835931ab34b1e002633
zidanhafiz <nadizsonata@gmail.com>
2024-02-25 11:38:52 +0800

    Add mixins and variables scss

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <p>NUMBER ONE CREATIVE DIGITAL AGENCY</p>
       <h1>Smart Choices For Your Business Growth </h1>
     </main>
   );
