13130ba8f4f5072d6f6e09483174a706f0463b40
zidanhafiz <nadizsonata@gmail.com>
2024-02-25 12:38:48 +0800

    Add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/button';
 import styles from './page.module.scss';
 
 export default function Home() {
@@ -5,6 +6,13 @@ export default function Home() {
     <main className={styles.main}>
       <p>NUMBER ONE CREATIVE DIGITAL AGENCY</p>
       <h1>Smart Choices For Your Business Growth </h1>
+      <Button
+        type='button'
+        size='lg'
+        icon
+      >
+        Contact Us
+      </Button>
     </main>
   );
 }
---
+++ components/button/index.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { ReactNode } from 'react';
+import { clsx } from 'clsx';
+import { HTMLMotionProps, Variants, motion } from 'framer-motion';
+import Link from 'next/link';
+import './index.scss';
+import Image from 'next/image';
+
+type ButtonProps = {
+  children: ReactNode;
+  type: 'button' | 'a';
+  size: 'sm' | 'md' | 'lg';
+  icon?: boolean;
+  href?: string;
+  variants?: Variants;
+};
+
+const Button = ({ children, type, size, href, icon, ...props }: ButtonProps) => {
+  if (type === 'button') {
+    return (
+      <motion.button
+        className={clsx('button', size)}
+        {...props}
+      >
+        {children}
+        {icon && (
+          <Image
+            className={clsx('icon', size)}
+            src='/icons/arrow-right.png'
+            width={24}
+            height={24}
+            alt='arrow'
+          />
+        )}
+      </motion.button>
+    );
+  }
+
+  return (
+    <motion.div {...props}>
+      <Link
+        className={clsx('button', size)}
+        href={href ? href : '#'}
+      >
+        {children}
+        {icon && (
+          <Image
+            className={clsx('icon', size)}
+            src='/icons/arrow-right.png'
+            width={24}
+            height={24}
+            alt='arrow'
+          />
+        )}
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Button;
